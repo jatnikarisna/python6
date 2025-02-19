9906d6031bceda61c4beb10bf746fdc9915b1fd6
jatnikarisna <jatnikarisna@gmail.com>
2025-02-19 23:07:09 +0700

    sleep

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, World! Please wath "The Pursuit" :) v1"
+    return "Hello, World! Please wath The Pursuit"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
