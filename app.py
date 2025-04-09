269c7f656195268cf549d9daadae2dfc1eb9d7ad
jatnikarisna <jatnikarisna@gmail.com>
2025-04-09 14:18:55 +0700

    04092025 :)

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello, Ericsson World! Please watch The Pursuit now 04092025 :)"
+    return "Hello, Ericsson World! Please watch The Pursuit now 04092025"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
